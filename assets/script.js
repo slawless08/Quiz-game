6bdb7caeb01e20df1242198f2faaa8a27bbfe76a
slawless08 <samuel.l.lawless@gmail.com>
2021-05-17 17:57:02 -0400

    Experimenting with defining objects and storing them as arrays to use for the questions and answers.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,23 @@
 var startButton = document.querySelector("#start");
 var gameTitle = document.querySelector("#game-title");
 var gameDescr = document.querySelector("#game-desc");
-var quizQuesitons = ["Should I get another sparkling water?"];
-var quizAnswers = ["Yes", "No", "Maybe", "Possibly not"];
+var quizQuesitons = [questions];
+var quizAnswers = [questionOneAnswers];
 var listAnswers = document.querySelector("#answers");
 var disappear = document.querySelector("#disappear");
 var list = document.createElement("ul");
+
+var questions = {
+    one: "Should I get a sparkling water?",
+    two: "Does this question work?",
+    three: "Will this one work too?",
+    four: "Last but not least?"
+}
+
+var questionOneAnswers = ["yes","no","maybe so"];
+
+console.log(questions);
+console.log(questionOneAnswers);
 
 startButton.addEventListener('click', function () {
     gameTitle.textContent = "";
@@ -16,10 +28,10 @@ startButton.addEventListener('click', function () {
 })
 
 function startQuiz() {
-    gameTitle.textContent = quizQuesitons;
+    gameTitle.textContent = questions.one;
 
     document.body.appendChild(list)
-    quizAnswers.forEach(function (e) {
+    questionOneAnswers.forEach(function (e) {
         var li = document.createElement("li");
         li.innerText = e;
         console.log(li);
